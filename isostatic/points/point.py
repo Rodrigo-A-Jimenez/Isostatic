0e55f81020a34bfe8f0ca08f8cd7ccd0cf0402ea
AlphyDev <rodrigojimenez@alphystica.com>
2022-02-19 18:04:18 -0400

    V0.0.1dev3 - typePoint - rotate

--- isostatic/points/point.py
+++ isostatic/points/point.py
@@ -1,14 +1,19 @@
 
 
 class Point:
-    def __init__(self, *args) -> None:
+    def __init__(self, *args, typePoint = 'Point', rotate_degrees = 0, ) -> None:
         self.args = [*args]
+        self.typePoint = typePoint
+        self.rotate_degrees = rotate_degrees #degrees
 
     def __str__(self) -> str:
-        return (r'Point: {}'.format(self.args))
+        return (r'{}: {}'.format(self.typePoint, self.args))
 
     def __repr__(self) -> str:
-        return (r'Point: {}'.format(self.args))
+        return (r'{}: {}'.format(self.typePoint, self.args))
 
     def __len__(self) -> int:
         return len(self.args)
+
+    def components(self):
+        pass
