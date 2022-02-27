ec9984ab5bcabec07c8e6659a68b134be5910c25
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-02-26 22:37:39 -0400

    docstring PuntualLoad

--- isostatic/rotations/angles.py
+++ isostatic/rotations/angles.py
@@ -66,6 +66,12 @@ class GeneralLoadLineal:
 
 class PuntualLoad():
     def __init__(self, puntualLoad:float, a:float, b:float, E=modE, I = I_) -> None:
+        '''
+        Classe que soporta la creacion de cargas puntuales
+        puntualLoad: carga puntual aplicada
+        a: distancia hacia el apoyo izquierdo de la carga puntual
+        b: distancia hacia el apoyo derecho de la carga puntual
+        '''
         self._L = a + b
         self._a = a
         self._b = b
