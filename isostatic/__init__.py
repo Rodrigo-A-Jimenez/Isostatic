ad74c2d1c6fa96ee460a8dfa7530e53186c5d810
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-03-06 16:23:29 -0400

    Refactorizacion de nombre LOADS

--- isostatic/__init__.py
+++ isostatic/__init__.py
@@ -1,6 +1,6 @@
 
 from .points.point import Point
 from .supports.support import Support
-from .rotations.angles import LinealLoad, GeneralLoadLineal, PuntualLoad
+from .loads.loads import LinealLoad, GeneralLoadLineal, PuntualLoad
 from .constants.constantsSymbols import Inertia, E
 from .elements.beams import Beam
