fc4f45a5f9e1361f4434ca1af4eae35416655372
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-03-19 18:50:56 -0400

    añadiendo archivos

---
+++ isostatic/core/static.py
@@ -0,0 +1,11 @@
+'''
+*******************************************
+*                                         *
+* author : Rodrigo Jimenez                *
+* E-mail : jimenezhuancarodrigo@gmail.com *
+* copyright : (c) 2022                    *
+* date : 2022                             *
+*                                         *
+*******************************************
+'''
+
--- isostatic/core/system.py
+++ isostatic/core/system.py
@@ -1,3 +1,14 @@
+'''
+*******************************************
+*                                         *
+* author : Rodrigo Jimenez                *
+* E-mail : jimenezhuancarodrigo@gmail.com *
+* copyright : (c) 2022                    *
+* date : 2022                             *
+*                                         *
+*******************************************
+'''
+
 from isostatic import X, Y, Z
 
 
