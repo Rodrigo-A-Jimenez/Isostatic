0e842922f99eec84c03a94577828504a39bed41b
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-03-20 23:33:42 -0400

    creacion de archivo vectors.py

---
+++ isostatic/points/vectors.py
@@ -0,0 +1,12 @@
+'''class Vector se definira para los vectores fuerza
+se puede definir: 
+- a traves de un modulo y direccion,
+- a traves de dos puntos (pero los puntos de este paquete son articulaciones en la estructura)
+- 
+
+'''
+
+
+class Vector():
+    def __init__(self, dim: int, value: list|float, implent: list,direction = 'gravitational') -> None:
+        pass
