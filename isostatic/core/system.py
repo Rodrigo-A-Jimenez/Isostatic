420ac208bc581d8771f6d5197ecb41d5e1a9eba3
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-03-28 13:42:37 -0400

    adding method summation in a System

--- isostatic/core/system.py
+++ isostatic/core/system.py
@@ -10,16 +10,26 @@
 '''
 
 from isostatic import X, Y, Z
+from isostatic.points.vectors import Vector
 
-
+def summationVectors(*args:Vector) -> Vector:
+    '''
+    This function receives a list of vectors and returns the sum of all of them.
+    '''
+    __result = Vector(0, [0, 0])
+    for i in args:
+        __result += i
+    return __result
 class CoordinateSystem():
-    def __init__(self, element: object)-> None:
+    def __init__(self, element: object, forces:list = [])-> None:
         self.__X = X
         self.__Y = Y
         self.__Z = Z
         self.__origin = element.initialPoint
         self.__endSystem = element.endPoint
         self.__unitVector = element.modulusVector/element.modulusVector.modulus()
+        
+        self.__forces = forces
         
     @property
     def x(self):
@@ -35,3 +45,8 @@ class CoordinateSystem():
     
     def interval(self ,begin, ending):
         return [begin*self.__X , ending*self.__Y]
+    
+    def addForce(self, force):
+        self.__forces.append(force)
+        
+        
--- isostatic/points/vectors.py
+++ isostatic/points/vectors.py
@@ -1,75 +1,71 @@
 
-from isostatic.core.system import CoordinateSystem
 from sympy import atan, cos, pi, sin
 
 
 class Vector():
-    def __init__(self, pointX, module: float|list , gravitational:bool = True ,inclination:float=pi/2) -> None:
+    def __init__(self, pointX, module: float | list, gravitational: bool = True, inclination: float = pi/2) -> None:
         if not isinstance(gravitational, bool):
             raise TypeError("gravitational must be a boolean")
-        
+
         if isinstance(module, float) or isinstance(module, int):
             self.__mod = module
             self.__inclination = inclination
-            
+
             self.__notation = []
             if inclination == pi/2:
                 self.__notation.append(0)
             else:
                 self.__notation.append(-self.__mod * cos(inclination))
-            
+
             if gravitational:
                 self.__notation.append(-self.__mod * sin(inclination))
             else:
                 self.__notation.append(-self.__mod * sin(inclination))
-                
+
         elif isinstance(module, list):
             self.__mod = 0
             for i in module:
                 self.__mod += i**2
             self.__mod = self.__mod**0.5
-            
+
             self.__notation = module
             self.__inclination = atan(module[1]/module[0])
-            
-        
+
         self.__implementation = pointX
-        
+
         self.__gravitational = gravitational
-    
+
     def forceMoment(self):
         return self.implementation * self.__notation[1]
-         
+
     @property
     def notation(self):
         return self.__notation
-    
+
     @property
     def module(self):
         return self.__mod
-    
+
     @property
     def inclination(self):
         return self.__inclination
-    
+
     @property
     def implementation(self):
         return self.__implementation
-    
+
     @property
     def gravitational(self):
         return self.__gravitational
-    
-    
+
     def __add__(self, __o: object) -> object:
         if len(self.__notation) != len(__o.notation):
             raise ValueError("The vectors must have the same size")
-        __result = [x+y for x,y in zip(self.notation, __o.notation)]
+        __result = [x+y for x, y in zip(self.notation, __o.notation)]
         return Vector(self.implementation, __result, self.gravitational)
-    
-    def __sub__(self, __o: object)-> object:
+
+    def __sub__(self, __o: object) -> object:
         if len(self.__notation) != len(__o.notation):
             raise ValueError("The vectors must have the same size")
-        __result = [x-y for x,y in zip(self.notation, __o.notation)]
+        __result = [x-y for x, y in zip(self.notation, __o.notation)]
         return Vector(self.implementation, __result, self.gravitational)
-    
